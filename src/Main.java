31f32c24cb95d606ce17aeed8caf04847d9a806b
NKspec <nkspec@yandex.ru>
2023-03-29 20:57:11 +0300

    change code

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,13 @@
 public class Main {
     public static void main(String[] args) {
         int cost = 10039;
-        int MilesRule = 20;
-        int bonus = cost / MilesRule;
-        if ((bonus * MilesRule) > cost) {
+        int milesrule = 20;
+        int bonus = cost / milesrule;
+        if ((bonus * milesrule) > cost) {
             System.out.println(bonus - 1);
         }
+        System.out.println("Ваш бонус:");
         System.out.println(bonus);
+        System.out.println("руб.");
     }
 }
